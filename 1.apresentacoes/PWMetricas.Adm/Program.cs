57b1fa4a5642e0acdefe06ddc2315a4594f4b98f
Mariane Ribeiro <mari.ribeiro1994@gmail.com>
2025-04-20 00:19:41 -0300

    inicio alteração layout

--- 1.apresentacoes/PWMetricas.Adm/Program.cs
+++ 1.apresentacoes/PWMetricas.Adm/Program.cs
@@ -40,6 +40,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+        ctx.Context.Response.Headers["Pragma"] = "no-cache";
+        ctx.Context.Response.Headers["Expires"] = "0";
+    }
+});
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
