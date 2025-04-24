d93f6d419e8b6858a32ee0388164d5d8d45c3c8b
Mariane Ribeiro <mari.ribeiro1994@gmail.com>
2025-04-24 00:47:10 -0300

    inicio crud filiais/lojas

---
+++ 1.apresentacoes/PWMetricas.Adm/Controllers/LojaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PWMetricas.Adm.Controllers
+{
+    [Authorize]
+    public class LojaController : Controller
+    {
+        public LojaController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
